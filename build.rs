80f84ac08f2f266c8e0c542e58cd712f053da5dc
Benjamin Frisby <frisbybenjamin@gmail.com>
2026-09-16 19:22:47 -0400

    Use the vcpkg libuv archive name

--- build.rs
+++ build.rs
@@ -150,12 +150,13 @@ fn configure_windows_dependencies(builder: &mut cc::Build) {
 
     builder.include(include_path);
     println!("cargo:rustc-link-search=native={}", library_path.display());
-    println!("cargo:rustc-link-lib=static=uv_a");
+    println!("cargo:rustc-link-lib=static=libuv");
     println!("cargo:rustc-link-lib=static=yaml-cpp");
 
     // Transitive Windows libraries required by a static libuv build.
     for library in [
-        "advapi32", "iphlpapi", "psapi", "shell32", "user32", "userenv", "ws2_32",
+        "advapi32", "dbghelp", "iphlpapi", "ole32", "psapi", "shell32", "user32", "userenv",
+        "ws2_32",
     ] {
         println!("cargo:rustc-link-lib={}", library);
     }
